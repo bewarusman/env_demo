1c624c018778498c644450989214d53e07074f95
bewar <bewardevelopment@yahoo.com>
2021-10-27 12:12:52 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const http = require("http");
+
+const ENV_VARS = {
+  dbHost: process.env.DB_HOST,
+  dbUser: process.env.DB_USER,
+  dbPass: process.env.DB_PASS,
+  email: process.env.EMAIL,
+};
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.write(JSON.stringify(ENV_VARS));
+    res.end();
+  })
+  .listen(8080);
